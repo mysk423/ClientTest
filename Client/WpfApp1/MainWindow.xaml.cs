6e2dd108aa5910873f046df7c7c2d4fa95aee455
mysk423 <80307373+mysk423@users.noreply.github.com>
2024-09-03 23:47:38 +0900

    0903
    - ip combobox 추가
    - 소켓 생성부분 작업중

--- Client/WpfApp1/MainWindow.xaml.cs
+++ Client/WpfApp1/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net.WebSockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +22,55 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<String> ServerList { get; set; } = new List<String>(){ "ip" };
+        private String ServerIP = null;
+        private Client client;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            ComboBoxList.ItemsSource = ServerList;
+        }
+
+        private void ComboBoxList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(ComboBoxList.SelectedItem != null)
+            {
+                ServerIP = ComboBoxList.SelectedItem.ToString();
+            }
+        }
+
+        private void ServerConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(ServerIP != null)
+            {
+                client = new Client(ServerIP);
+            }
+            else
+            {
+                MessageBox.Show("Do not Select Server!!!!");
+            }
+        }
+
+        public class Client
+        {
+            private WebSocket testWebSocket;
+            public Client(String ServerIP)
+            {
+                StartConnect(ServerIP);
+            }
+
+            public async void StartConnect(String ServerIP)
+            {
+                ClientWebSocket ws = new ClientWebSocket();
+                Uri uri = new Uri(ServerIP);
+
+                await ws.ConnectAsync(uri, System.Threading.CancellationToken.None);
+            }
         }
     }
+
 }
+
+
